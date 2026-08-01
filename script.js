443ca87c748b75d4da891c9bc5eb04dbc4aec8ab
wyind <69166653+Wyind@users.noreply.github.com>
2026-07-31 23:05:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('navbar');
-    if (navbar) {
-        window.addEventListener('scroll', () => {
-            navbar.classList.toggle('scrolled', window.scrollY > 50);
-        });
-    }
-});
